8dd63b98e1ae27e2fe470f32c2e28fd7fd05a0fa
montoya <gabi.montoya@gmail.com>
2018-02-27 16:19:11 +0100

    Section_2: 6,7,8,9,10 and 11 Variables and Mehtods

---
+++ section_02/variables_methods.py
@@ -0,0 +1,30 @@
+a = 5
+b = 10
+my_variable = 56
+
+string_variable = "hello"
+string_quotes = 'string can have single quotes'
+
+# print (my_variable)
+# print (string_variable)
+# print ("Hello world!")
+# print (123)
+
+##
+
+def my_print_method(my_argument):
+    print(my_argument)
+
+
+my_print_method("Hola, Mundo")
+
+
+def my_multiply_method (number_1, number_2):
+    return number_1 * number_2
+
+
+result = my_multiply_method (5, 3)
+my_print_method (result)
+
+
+
