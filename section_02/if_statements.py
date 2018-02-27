10492f7511176559eb8d504d0d5d51933f1f4f67
montoya <gabi.montoya@gmail.com>
2018-02-27 17:57:12 +0100

    Section_2: 17 If statement, string.format()

---
+++ section_02/if_statements.py
@@ -0,0 +1,20 @@
+# if
+
+should_continue = True
+
+if should_continue:
+    print("Hello")
+
+## if XX [not] in
+
+know_people = ["John", "Anna", "Mary"]
+person = input("Enter the person you know: ")
+
+if person in know_people:
+    print("You know {} !!".format(person))
+else:
+    print("You don´t know {}".format(person))
+
+
+
+
