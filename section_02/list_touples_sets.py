8b794a5423190b40fcb579d98d0ab020b1aa21b3
montoya <gabi.montoya@gmail.com>
2018-02-27 17:08:13 +0100

    Section_2: 14 Advance set operations

--- section_02/list_touples_sets.py
+++ section_02/list_touples_sets.py
@@ -46,4 +46,21 @@ set_grades.add (61)
 print(set_grades)
 
 
+## Advance set operations
+
+your_lottery_numbers = {1, 2, 3, 4, 5, 12}
+winning_numbers = {1, 3, 5, 7, 9, 11}
+
+print (your_lottery_numbers.intersection(winning_numbers))
+print (your_lottery_numbers.union(winning_numbers))
+
+print ({1, 2, 3, 4}.difference( {1, 2} ))
+
+
+
+
+
+
+
+
 
