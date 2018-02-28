3c4479c02ac3a3bc55d4b7e33e5eaea41e556cc2
montoya <gabi.montoya@gmail.com>
2018-02-28 14:55:56 +0100

    Section_2: 25 Student class

--- section_02/classes_objects.py
+++ section_02/classes_objects.py
@@ -23,3 +23,25 @@ print(player_one.total())
 
 print(player_one == player_two)
 print(player_one.name == player_two.name)
+
+
+## Student class
+
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+    
+    def average (self):
+        return sum(self.marks)/len(self.marks)
+    
+
+
+anna = Student("Anna", "MIT")
+anna.marks.append(56)
+anna.marks.append(71)
+print(anna.marks)
+print(anna.average())
+
+
