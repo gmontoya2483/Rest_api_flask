d2bb07551ad70be008833d1720d8bcf51f2fe0d5
montoya <gabi.montoya@gmail.com>
2018-02-28 12:04:18 +0100

    Se agrego el ejercicio 19 - string.split(), string.strip()

---
+++ section_02/programming_excercise_19.py
@@ -0,0 +1,43 @@
+
+
+## if XX [not] in
+
+#know_people = []
+# person = input("Enter the person you know: ")
+
+
+
+## Excersice 19
+
+
+def who_do_you_know():
+    # Ask the user for a list of people they know
+    persons = input("Enter the list of persons you know, separated by commas: ")
+    # Split the string into a list
+    list_persons_with_spaces = persons.split(",")
+    
+    # remove the spaces
+    list_persons_without_spaces = []
+    for person in list_persons_with_spaces:
+        list_persons_without_spaces.append(person.strip())
+
+    # Return that list
+    return list_persons_without_spaces
+
+def ask_user():
+    # Ask user for a name
+    person = input("Enter a name of someone you know: ")
+    # See if their name is in the list of people they know
+    # Print out that they know the persosn
+    if person in who_do_you_know():
+        print("You know {} !!".format(person))
+    else:
+        print("You don´t know {}".format(person))
+
+
+ask_user()
+
+
+
+
+
