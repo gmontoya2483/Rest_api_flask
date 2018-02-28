721c6921c89c1259d313d5109f30faf3a437cf90
montoya <gabi.montoya@gmail.com>
2018-02-28 15:32:09 +0100

    Section_2: 27 @classmethod and @staticmethod

--- section_02/classes_objects.py
+++ section_02/classes_objects.py
@@ -36,12 +36,21 @@ class Student:
     def average (self):
         return sum(self.marks)/len(self.marks)
     
+    @classmethod
+    def go_to_school(cls):
+        print("I'm going to school.")
+        print("I'm a {}".format(cls))
+    
+    @staticmethod
+    def comming_back_from_school():
+        print("I'm comming back from school.")
 
+    
 
 anna = Student("Anna", "MIT")
 anna.marks.append(56)
 anna.marks.append(71)
 print(anna.marks)
 print(anna.average())
-
-
+Student.go_to_school()
+Student.comming_back_from_school()
