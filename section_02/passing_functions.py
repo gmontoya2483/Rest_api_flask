16008dac8a40bf39618af9d5f39fb1ab2178804b
montoya <gabi.montoya@gmail.com>
2018-03-02 11:07:44 +0100

    Section_2: 31 Passing functions as arguments

---
+++ section_02/passing_functions.py
@@ -0,0 +1,31 @@
+# Passing functions as parameters
+# Generalize code
+
+def methodception(another):
+   return another()
+
+
+def add_two_numbers():
+    return 35 + 77
+
+
+print(methodception(add_two_numbers))
+
+print(methodception(lambda: 35 + 77)) # lambda -> anonymus function
+
+my_list = [13, 56, 77, 484]
+print(list(filter(lambda x: x !=13, my_list)))
+
+
+## lambda tranlation to normal functions
+def not_thirteen(x):
+    return x != 13
+print(list(filter(not_thirteen,my_list)))
+
+## translate lambda to list comprehansion
+print ([x for x in my_list if x != 13])
+
+
+
+
+
