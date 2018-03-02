9e304981df4538994d90ef03c93758dd551310a6
montoya <gabi.montoya@gmail.com>
2018-03-02 13:12:11 +0100

    Section_2: 33 Advance decorators, parameters

--- section_02/decorators.py
+++ section_02/decorators.py
@@ -8,14 +8,51 @@ def my_decorator(func):
     def function_that_runs_func():
         print("In the decorator!")
         func()
-        print ("After the decorator!")
+        print ("After the decorator!\n")
     return function_that_runs_func
 
 
 @my_decorator
 def my_function():
     print("I´m the function!!!")
-
-
+    
 my_function()
+
+
+## Decorators with arguments, suma un nivel al wrapping
+
+def decorator_with_arguments(number):
+    def my_decorator(func):
+        @functools.wraps(func)
+        def function_that_runs_func():
+            print("In the decorator, decorator parameter: {}".format(number))
+            func()
+            print("After the decorator, decorator parameter: {}\n".format(number))
+        return function_that_runs_func
+    return my_decorator
+
+
+@decorator_with_arguments(56)
+def my_function_too():
+    print("Hello")
+
+my_function_too()
+
+
+## decoradores with arguments in the function, agrega *args, **kwargs
+
+def my_decorator_three(func):
+    @functools.wraps(func)
+    def function_that_runs_func(*args, **kwargs):
+        print("In the decorator!")
+        func(*args, **kwargs)
+        print ("After the decorator!\n")
+    return function_that_runs_func
+
+
+@my_decorator_three
+def my_function_with_parameters(number_1, number_2):
+    print("I´m the function!!!, the sum is {}".format(number_1 + number_2))
+    
+my_function_with_parameters(3,5)
 
