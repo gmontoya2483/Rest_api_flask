cb824e8fabd86d83ca4a7eac9e4a6a6ca4e34516
montoya <gabi.montoya@gmail.com>
2018-03-12 19:13:29 +0100

    Section 6, Se modifico el modelo Item para usar SQLalchemy

--- section_06_Flask_SQLAlchemy/code/resources/item.py
+++ section_06_Flask_SQLAlchemy/code/resources/item.py
@@ -30,7 +30,7 @@ class Item(Resource):
 
         item = ItemModel(name, data['price'])
         try:
-            item.insert()
+            item.save_to_db()
         except:
             return {'message': "An error occurred inserting the item."},500
 
@@ -38,17 +38,9 @@ class Item(Resource):
 
 
     def delete(self, name):
-        if ItemModel.find_by_name(name) is None:
-            return {'message': "An item with name '{}' does not exist".format(name)}, 400
-
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "DELETE FROM items WHERE name=?"
-        cursor.execute(query,(name,))
-
-        connection.commit()
-        connection.close()
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
         
         return {'message': 'Item deleted'}
 
@@ -56,20 +48,14 @@ class Item(Resource):
     def put (self, name):
         data = Item.parser.parse_args()
         item = ItemModel.find_by_name(name)
-        updated_item = ItemModel(name, data['price'])
-
+        
         if item is None:
-            try:
-                updated_item.insert()
-            except:
-                return {'message': "An error occurred inserting the item."},500
-            return updated_item.json(), 201
+            item = ItemModel(name, data['price'])
         else:
-            try:
-                updated_item.update()
-            except:
-                return {'message': "An error occurred updating the item."},500
-            return updated_item.json()
+            item.price = data['price']
+        
+        item.save_to_db()
+        return item.json()
 
 class ItemList(Resource):
 
@@ -83,7 +69,7 @@ class ItemList(Resource):
         result = cursor.execute(query)
 
         for row in result:
-            item = {'name': row[0], 'price':row[1]}
+            item = {'name': row[1], 'price':row[2]}
             list_of_items.append(item)
         
         connection.close()
